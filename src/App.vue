026c0f8352911b0d71707cac6f2a29fc8afb3aaf
codemaoz90 <codemaoz90@gmail.com>
2021-11-10 20:05:39 +0100

    Markdown finished

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,71 @@
 <template>
   <div id="notebook">
+    <!-- Sidebar -->
     <aside class="side-bar">
-      <!-- Toolbar -->
       <div class="toolbar">
-        <!-- Add note button -->
-        <button :title="addButtonTitle" @click="addNote">
+        <button @click="addNote" :title="notes.length + ' note(s) already'">
           <i class="material-icons">add</i> Add note
         </button>
       </div>
-      <!-- Here will be the note list -->
-
       <div class="notes">
         <div
           class="note"
-          v-for="(note, index) of notes"
-          @click="selectNote(note)"
+          v-for="(note, index) of sortedNotes"
           :key="index"
+          :class="{ selected: note === selectedNote }"
+          @click="selectNote(note)"
         >
-          {{ note.title }}
+          <i class="icon material-icons" v-if="note.favorite">star</i
+          >{{ note.title }}
         </div>
       </div>
     </aside>
-    <!-- Main pane -->
-    <section class="main">
-      <textarea v-model="content"></textarea>
-    </section>
 
-    <!-- Preview pane -->
-    <aside class="preview" v-html="notePreview"></aside>
+    <template v-if="selectedNote">
+      <!-- Main pane -->
+      <section class="main">
+        <div class="toolbar">
+          <input v-model="selectedNote.title" placeholder="Note title" />
+
+          <button @click="favoriteNote" title="Favorite note">
+            <i class="material-icons">{{
+              selectedNote.favorite ? "star" : "star_border"
+            }}</i>
+          </button>
+
+          <button @click="removeNote" title="Remove note">
+            <i class="material-icons">delete</i>
+          </button>
+        </div>
+        <textarea v-model="selectedNote.content"></textarea>
+        <div class="toolbar status-bar">
+          <span class="date">
+            <span class="label">Created</span>
+            <span class="value">{{ selectedNote.created | date }}</span>
+          </span>
+          <span class="lines">
+            <span class="label">Lines</span>
+            <span class="value">{{ linesCount }}</span>
+          </span>
+          <span class="words">
+            <span class="label">Words</span>
+            <span class="value">{{ wordsCount }}</span>
+          </span>
+          <span class="characters">
+            <span class="label">Characters</span>
+            <span class="value">{{ charactersCount }}</span>
+          </span>
+        </div>
+      </section>
+
+      <!-- Preview pane -->
+      <aside class="preview" v-html="notePreview"></aside>
+    </template>
   </div>
 </template>
 
 <script>
+/* eslint-disable vue/return-in-computed-property */
 import "material-icons/iconfont/material-icons.css"
 import { marked } from "marked"
 export default {
@@ -39,31 +73,80 @@ export default {
   components: {
     // HelloWorld,
   },
-  created() {
-    this.content =
-      localStorage.getItem("content") || "You can write in **Markdown**"
-  },
+
   data() {
     return {
-      content: "This is a note. **marked**",
-      notes: [],
-      selectedId: null
+      // These are loaded from localStorage and have a default value
+      // Don't forget the JSON parsing for the notes array
+      notes: JSON.parse(localStorage.getItem("notes")) || [],
+      selectedId: localStorage.getItem("selected-id") || null
     }
   },
+
+  // Computed properties
   computed: {
+    selectedNote() {
+      // We return the matching note with selectedId
+      return this.notes.find((note) => note.id === this.selectedId)
+    },
+
     notePreview() {
       // Markdown rendered to HTML
-      return marked(this.content)
+      return this.selectedNote ? marked(this.selectedNote.content) : ""
     },
-    addButtonTitle() {
-      return this.notes.length + " note(s) already"
+
+    sortedNotes() {
+      return this.notes
+        .slice()
+        .sort((a, b) => a.created - b.created)
+        .sort((a, b) => (a.favorite === b.favorite ? 0 : a.favorite ? -1 : 1))
+    },
+
+    linesCount() {
+      if (this.selectedNote) {
+        // Count the number of new line characters
+        return this.selectedNote.content.split(/\r\n|\r|\n/).length
+      }
+    },
+
+    wordsCount() {
+      if (this.selectedNote) {
+        var s = this.selectedNote.content
+        // Turn new line cahracters into white-spaces
+        s = s.replace(/\n/g, " ")
+        // Exclude start and end white-spaces
+        s = s.replace(/(^\s*)|(\s*$)/gi, "")
+        // Turn 2 or more duplicate white-spaces into 1
+        s = s.replace(/[ ]{2,}/gi, " ")
+        // Return the number of spaces
+        return s.split(" ").length
+      }
+    },
+
+    charactersCount() {
+      if (this.selectedNote) {
+        return this.selectedNote.content.split("").length
+      }
     }
   },
-  methods: {
-    saveNote(val) {
-      console.log("saving note", val)
-      localStorage.setItem("content", val)
+
+  // Change watchers
+  watch: {
+    // When our notes change, we save them
+    notes: {
+      // The method name
+      handler: "saveNotes",
+      // We need this to watch each note's properties inside the array
+      deep: true
     },
+    // Let's save the selection too
+    selectedId(val) {
+      localStorage.setItem("selected-id", val)
+    }
+  },
+
+  methods: {
+    // Add a note with some default content and select it
     addNote() {
       const time = Date.now()
       // Default new note
@@ -71,21 +154,43 @@ export default {
         id: String(time),
         title: "New note " + (this.notes.length + 1),
         content:
-          "**Hi!** This notebook is using[markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!",
+          "**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!",
         created: time,
         favorite: false
       }
-      // Add to the list
+      // Add
       this.notes.push(note)
+      // Select
+      this.selectNote(note)
     },
+
+    // Remove the selected note and select the next one
+    removeNote() {
+      if (this.selectedNote && confirm("Delete the note?")) {
+        // Remove the note in the notes array
+        const index = this.notes.indexOf(this.selectedNote)
+        if (index !== -1) {
+          this.notes.splice(index, 1)
+        }
+      }
+    },
+
     selectNote(note) {
+      // This will update the 'selectedNote' computed property
       this.selectedId = note.id
-      console.log(this.selectedId)
+    },
+
+    saveNotes() {
+      // Don't forget to stringify to JSON before storing
+      localStorage.setItem("notes", JSON.stringify(this.notes))
+      console.log("Notes saved!", new Date())
+    },
+
+    favoriteNote() {
+      // this.selectedNote.favorite = !this.selectedNote.favorite
+      // this.selectedNote.favorite = this.selectedNote.favorite ^ true
+      this.selectedNote.favorite ^= true
     }
-  },
-  watch: {
-    // Watching the "content" data property!
-    content: "saveNote"
   }
 }
 </script>
